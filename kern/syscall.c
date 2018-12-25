c46fbeb5b5784cf63b14d753f3702006f166fff7
LiuZichen <danteliuzichen@163.com>
2018-12-25 19:38:05 +0800

    partB 练习12

--- kern/syscall.c
+++ kern/syscall.c
@@ -405,6 +405,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 
 	switch (syscallno) {
 		// lab4:
+		case SYS_env_set_pgfault_upcall:
+			return sys_env_set_pgfault_upcall(a1,(void*)a2);
 		case SYS_exofork:
 			return sys_exofork();
 		case SYS_env_set_status:
--- lib/pgfault.c
+++ lib/pgfault.c
@@ -30,10 +30,10 @@ set_pgfault_handler(void (*handler)(struct UTrapframe *utf))
 		// First time through!
 		// LAB 4: Your code here.
 		// panic("set_pgfault_handler not implemented");
-		r = sys_page_alloc(0, (void*)(UXSTACKTOP - PGSIZE), (PTE_U|PTE_P|PTE_W) < 0);
+		r = sys_page_alloc(0, (void*)(UXSTACKTOP - PGSIZE), (PTE_U|PTE_P|PTE_W));
 		if(r < 0)
 		{
-			panic("set_pgfault_handler: %d", r);
+			panic("set_pgfault_handler: %e", r);
 		}
 		sys_env_set_pgfault_upcall(0, _pgfault_upcall);
 	}
